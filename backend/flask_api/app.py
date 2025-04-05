4190935b230584a235baa1d27d8941e7b3d284f6
NileDistributary <niledistributary@gmail.com>
2025-04-06 00:35:06 +0100

    [WIP] Drafted Backend files needed for Chat Integration

--- backend/flask_api/app.py
+++ backend/flask_api/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_restful import Api
-from routes.auth import Register, Login  
+# from flask_cors import CORS  # Uncomment if you want to enable CORS
+from routes.auth import Register, Login 
+from routes.chat import Chat 
 
 app = Flask(__name__)
+#CORS(app)  # Uncomment if you want to enable CORS
 api = Api(app)
 
 api.add_resource(Register, '/register')
 api.add_resource(Login, '/login')
+
+# Chatbot endpoint
+api.add_resource(Chat, '/chat')
 
 
 if __name__ == '__main__':
