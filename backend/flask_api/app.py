db63e26379682d5fe8ee3e375ee6ebdcd207696a
NileDistributary <niledistributary@gmail.com>
2025-04-06 04:09:02 +0100

    [WIP] Made sure the flask app gave access to the environment variable

--- backend/flask_api/app.py
+++ backend/flask_api/app.py
@@ -3,6 +3,12 @@ from flask_restful import Api
 # from flask_cors import CORS  # Uncomment if you want to enable CORS
 from routes.auth import Register, Login 
 from routes.chat import Chat 
+from dotenv import load_dotenv
+import os
+
+# Get the path to the .env file in the parent directory
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 #CORS(app)  # Uncomment if you want to enable CORS
